508fbe99a6d991a86fed0c2a855c670faa567365
Kopasuy <danilmertlukov@gmail.com>
2023-12-17 23:25:24 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,35 @@
 import numpy as np
 
+def create_array(size):
+    array = np.array([])
+    for i in range(size):
+        array = np.append(array, 0)
+    return array
+
+def array_from_out_array(out_array, iterations):
+    array = create_array(16)
+    for i in range(16):
+        array[i] = out_array[i + iterations]
+    return array
+
+def counting_iterations(out_array):
+    n_bits = len(out_array)
+    iterations = n_bits - 16
+    return iterations
+
 # model neuronet
 class Model:
     def __init__(self):
         self._hid_neuro = 16  # one hidden layer of neurons
-        self._previous_pred = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+        self._previous_pred = create_array(16)
+        self._out_array = []
         self._array = []
         self._weight0 = []
         self._weight1 = []
         self._iterations = 0
-        self._pred = self._previous_pred
+        self._pred = create_array(16)
 
-    def add_values(self):
-        pass
-
-    def counting_iterations(self):
+    def add_array(self):
         pass
 
     def predict(self):
@@ -25,23 +40,20 @@ class ModelCod(Model):
         self.__input_neuro = 16
         self.__output_neuro = 8192
 
-    def add_values(self, new_array, weight_to_16, weihgt_to_8192):
-        self._array.append(new_array)
-        self._weight0.append(weight_to_16)
-        self._weight1.append(weihgt_to_8192)
-
-    def counting_iterations(self):
-        n_bits = len(self._array)
-        self._iterations = n_bits / 16
-        return self._iterations
+    def add_array(self, new_array, weight_to_16, weight_to_8192):
+        self._out_array = new_array
+        self._weight0 = weight_to_16
+        self._weight1 = weight_to_8192
+        self._iterations = counting_iterations(self._out_array)
 
     def predict(self):
         for i in range(self._iterations):
-            self._pred = np.dot(self._array, self._weight_to_16)
-            self._pred = self._pred + self._previous_pred
+            self._array = array_from_out_array(self._out_array, i)
+            self._pred = np.dot(self._array, self._weight0)
             self._pred = np.tanh(self._pred)
+            self._pred += self._previous_pred
             self._previous_pred = self._pred
-        self._pred = np.dot(self._pred, self._weihgt_to_8192)
+        self._pred = np.dot(self._pred, self._weight1)
         return self._pred
 
 class ModelDecod(Model):
@@ -49,17 +61,17 @@ class ModelDecod(Model):
         self.__input_neuro = 8192
         self.__output_neuro = 16
 
-    def add_values(self, new_array, weight_to_8192, weiqht_to_16, iterations):
-        self._array.append(new_array)
-        self._weight0.append(weight_to_8192)
-        self._weight1.append(weiqht_to_16)
+    def add_array(self, new_array, weight_to_8192, weiqht_to_16, iterations):
+        self._out_array = new_array
+        self._weight0 = weight_to_8192
+        self._weight1 = weiqht_to_16
         self._iterations = iterations
 
     def predict(self):
-        self._pred = np.dot(self._array, self._weight0)
-        for i in range(self._iterations):
-            self._pred = self._pred + self._previous_pred
+        self._pred = np.dot(self._out_array, self._weight0)
+        for i in range(self._iterations + 1):
             self._pred = np.tanh(self._pred)
+            self._pred += self._previous_pred
             self._previous_pred = self._pred
             self._pred = np.dot(self._pred, self._weight1)
             return self._pred
@@ -70,4 +82,6 @@ class View:
 
 # controller of neuronet
 class Controller:
-    pass
+    def __init__(self):
+        self.code = ModelCod()
+        self.decode = ModelDecod()
