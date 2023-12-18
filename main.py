21b6c6a5bb288df1b50de8eb05be68497ca1e687
Kopasuy <danilmertlukov@gmail.com>
2023-12-18 22:02:54 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import numpy as np
 
+array_weight_2 = np.random.randn(2, 2)
+array_weight_1024 = np.random.randn(2, 1024)
+
 def create_array(size):
     array = np.array([])
     for i in range(size):
@@ -7,27 +10,27 @@ def create_array(size):
     return array
 
 def array_from_out_array(out_array, iterations):
-    array = create_array(16)
-    for i in range(16):
+    array = create_array(2)
+    for i in range(2):
         array[i] = out_array[i + iterations]
     return array
 
 def counting_iterations(out_array):
-    n_bits = len(out_array)
-    iterations = n_bits - 16
+    n_byte = len(out_array)
+    iterations = n_byte - 2
     return iterations
 
 # model neuronet
 class Model:
     def __init__(self):
-        self._hid_neuro = 16  # one hidden layer of neurons
-        self._previous_pred = create_array(16)
+        self._hid_neuro = 2  # one hidden layer of neurons
+        self._previous_pred = create_array(2)
         self._out_array = []
         self._array = []
         self._weight0 = []
         self._weight1 = []
         self._iterations = 0
-        self._pred = create_array(16)
+        self._pred = create_array(2)
 
     def add_array(self):
         pass
@@ -37,13 +40,13 @@ class Model:
 
 class ModelCod(Model):
     def __init__(self):
-        self.__input_neuro = 16
-        self.__output_neuro = 8192
+        self.__input_neuro = 2
+        self.__output_neuro = 1024
 
-    def add_array(self, new_array, weight_to_16, weight_to_8192):
+    def add_array(self, new_array, weight_to_2, weight_to_1024):
         self._out_array = new_array
-        self._weight0 = weight_to_16
-        self._weight1 = weight_to_8192
+        self._weight0 = weight_to_2
+        self._weight1 = weight_to_1024
         self._iterations = counting_iterations(self._out_array)
 
     def predict(self):
@@ -58,13 +61,13 @@ class ModelCod(Model):
 
 class ModelDecod(Model):
     def __init__(self):
-        self.__input_neuro = 8192
-        self.__output_neuro = 16
+        self.__input_neuro = 1024
+        self.__output_neuro = 2
 
-    def add_array(self, new_array, weight_to_8192, weiqht_to_16, iterations):
+    def add_array(self, new_array, weight_to_1024, weiqht_to_2, iterations):
         self._out_array = new_array
-        self._weight0 = weight_to_8192
-        self._weight1 = weiqht_to_16
+        self._weight0 = weight_to_1024
+        self._weight1 = weiqht_to_2
         self._iterations = iterations
 
     def predict(self):
@@ -78,10 +81,38 @@ class ModelDecod(Model):
 
 # view data for learning and etc
 class View:
-    pass
+    def code(self):
+        pass
+
+    def decode(self):
+        pass
+
+    def learn(self):
+        pass
+
+    def set_ip(self):
+        pass
 
 # controller of neuronet
 class Controller:
     def __init__(self):
         self.code = ModelCod()
         self.decode = ModelDecod()
+        self.view = View()
+
+    def specify_def(self):
+        while 1:
+            name_def = input()
+            match name_def:
+                case 'code':
+                    full_path = input('provide full path')
+                    array_byte = np.fromfile(full_path, dtype=np.byte)
+                    self.code.add_array(array_byte, array_weight_2, array_weight_1024)
+                case 'learn':
+                    pass
+                case 'set_ip':
+                    pass
+                case 'exit':
+                    break
+                case _:
+                    print('there is no such def')
